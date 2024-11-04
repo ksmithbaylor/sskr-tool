d9a150087ac8afd71d37efbddf323ea7007d17b1
Kevin Smith <ksmithbaylor@gmail.com>
2024-11-04 00:18:53 -0500

    Attempt recovery when shards from different universes are present

--- src/recover.rs
+++ src/recover.rs
@@ -32,99 +32,112 @@ pub fn recover(lines: Vec<String>) -> Result<Mnemonic, Error> {
         share_meta.push(meta);
     }
 
-    let identifier = share_ids[0];
+    // (id, threshold, count) => Vec<share_index>
+    let mut shares_by_universe: HashMap<(u16, usize, usize), Vec<usize>> = HashMap::new();
 
-    // Make sure identifier is the same for all shares
-    if share_ids.iter().any(|id| id != &identifier) {
-        bail!("Mismatched identifiers, shares don't go together");
-    }
-
-    let group_threshold = share_meta[0][1];
-    let group_count = share_meta[0][2];
-
-    // Make sure group threshold and group count are the same for all shares
-    if share_meta
-        .iter()
-        .any(|meta| meta[1] != group_threshold || meta[2] != group_count)
-    {
-        bail!("Mismatched group threshold or count, shares don't go together");
-    }
-
-    // Group shares by group in the form { group_num => Vec<(share_index, share)> }
-    let mut shares_by_group: HashMap<usize, Vec<(usize, Vec<u8>)>> = HashMap::new();
-    for (i, share) in shares.clone().iter().enumerate() {
-        let share_group_num = share_meta[i][0];
-
-        if !shares_by_group.contains_key(&share_group_num) {
-            shares_by_group.insert(share_group_num, vec![]);
+    for i in 0..shares.len() {
+        let key = (share_ids[i], share_meta[i][1], share_meta[i][2]);
+        if !shares_by_universe.contains_key(&key) {
+            shares_by_universe.insert(key, vec![]);
         }
 
-        shares_by_group
-            .get_mut(&share_group_num)
-            .unwrap()
-            .push((i, share.to_vec()));
+        shares_by_universe.get_mut(&key).unwrap().push(i);
     }
 
-    // See how many groups are recoverable
-    let mut recoverable_groups: Vec<usize> = vec![];
+    for ((identifier, group_threshold, _group_count), share_indices) in &shares_by_universe {
+        // Group shares by group in the form { group_num => Vec<(share_index, share)> }
+        let mut shares_by_group: HashMap<usize, Vec<(usize, Vec<u8>)>> = HashMap::new();
+        for &i in share_indices {
+            let share_group_num = share_meta[i][0];
 
-    // Look through the groups one-by-one to validate and gather information
-    for (group_num, shares) in &shares_by_group {
-        // Make sure the member threshold is the same for all shares in the group
-        let member_threshold = share_meta[shares[0].0][4];
-        if shares
-            .iter()
-            .any(|(i, _share)| share_meta[*i][4] != member_threshold)
-        {
-            bail!(
-                "Mismatched share member thresholds in group {}, shares don't go together",
-                group_num + 1
-            );
+            if !shares_by_group.contains_key(&share_group_num) {
+                shares_by_group.insert(share_group_num, vec![]);
+            }
+
+            shares_by_group
+                .get_mut(&share_group_num)
+                .unwrap()
+                .push((i, shares[i].to_vec()));
         }
 
-        // See whether this group is recoverable
-        if shares.len() >= member_threshold {
-            recoverable_groups.push(*group_num);
-        }
-    }
+        // See how many groups are recoverable
+        let mut recoverable_groups: Vec<usize> = vec![];
 
-    // Make sure there are enough groups to recover the secret
-    if recoverable_groups.len() < group_threshold {
-        bail!(
-            "Not enough groups, need to satisfy at least {} but only {} are satisfied ({})",
-            group_threshold,
-            recoverable_groups.len(),
-            recoverable_groups
+        // Look through the groups one-by-one to validate and gather information
+        for (group_num, shares) in &shares_by_group {
+            // Make sure the member threshold is the same for all shares in the group
+            let member_threshold = share_meta[shares[0].0][4];
+            if shares
                 .iter()
-                .map(|g| (g + 1).to_string())
-                .collect::<Vec<String>>()
-                .join(" and ")
-        )
-    }
+                .any(|(i, _share)| share_meta[*i][4] != member_threshold)
+            {
+                bail!(
+                    "Mismatched share member thresholds in group {}, shares don't go together",
+                    group_num + 1
+                );
+            }
 
-    let mut shares_for_recovery: Vec<Vec<u8>> = vec![];
+            // See whether this group is recoverable
+            if shares.len() >= member_threshold {
+                recoverable_groups.push(*group_num);
+            }
+        }
 
-    // Gather shares from enough theoretically-recoverable groups
-    for group_num in recoverable_groups.iter().take(group_threshold) {
-        // Get just the shares without the group number
-        let group_shares = shares_by_group[&group_num]
-            .iter()
-            .map(|(_i, share)| share.to_vec())
-            .collect::<Vec<Vec<u8>>>();
+        // Make sure there are enough groups to recover the secret
+        if recoverable_groups.len() < *group_threshold {
+            println!(
+                "{}: Not enough groups, need to satisfy at least {} but only {} are satisfied ({})",
+                identifier,
+                group_threshold,
+                recoverable_groups.len(),
+                recoverable_groups
+                    .iter()
+                    .map(|g| (g + 1).to_string())
+                    .collect::<Vec<String>>()
+                    .join(" and ")
+            );
+            continue;
+        }
 
-        for share in group_shares {
-            shares_for_recovery.push(share);
+        let mut shares_for_recovery: Vec<Vec<u8>> = vec![];
+
+        // Gather shares from enough theoretically-recoverable groups
+        for group_num in recoverable_groups.iter().take(*group_threshold) {
+            // Get just the shares without the group number
+            let group_shares = shares_by_group[&group_num]
+                .iter()
+                .map(|(_i, share)| share.to_vec())
+                .collect::<Vec<Vec<u8>>>();
+
+            for share in group_shares {
+                shares_for_recovery.push(share);
+            }
+        }
+
+        match sskr_combine(&shares_for_recovery) {
+            Ok(secret) => match Mnemonic::from_entropy(secret.data(), Language::English) {
+                Ok(mnemonic) => {
+                    println!("{}: Successfully recovered!", identifier);
+                    return Ok(mnemonic);
+                }
+                Err(e) => {
+                    println!(
+                        "{}: Recovered entropy 0x{} but unable to make mnemonic: {}",
+                        identifier,
+                        hex::encode(secret.data()),
+                        e
+                    );
+                    continue;
+                }
+            },
+            Err(e) => {
+                println!("{}: Error during SSKR combination: {}", identifier, e);
+                continue;
+            }
         }
     }
 
-    let secret = sskr_combine(&shares_for_recovery)
-        .map_err(|e| anyhow!("Error during SSKR combination: {}", e))?;
-
-    Mnemonic::from_entropy(secret.data(), Language::English).map_err(|e| {
-        anyhow!(
-            "Recovered entropy 0x{} but unable to make mnemonic: {}",
-            hex::encode(secret.data()),
-            e
-        )
-    })
+    Err(anyhow!(
+        "Not enough shares that belong together, unable to recover :("
+    ))
 }
